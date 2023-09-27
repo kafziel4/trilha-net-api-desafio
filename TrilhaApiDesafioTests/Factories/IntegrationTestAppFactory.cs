a7c7ddff3f334831ebe6e6415ea9c3675d261ca8
Keoma Kevin Baudin <keoma4@gmail.com>
2023-09-27 01:08:05 -0300

    test: implementa teste de integração ObterPorId

---
+++ TrilhaApiDesafioTests/Factories/IntegrationTestAppFactory.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Testcontainers.MsSql;
+using TrilhaApiDesafio.Context;
+
+namespace TrilhaApiDesafioTests.Factories
+{
+    public class IntegrationTestAppFactory<TStartup>
+        : WebApplicationFactory<TStartup>, IAsyncLifetime where TStartup : class
+    {
+        private readonly MsSqlContainer _msSqlcontainer = new MsSqlBuilder()
+            .WithImage("mcr.microsoft.com/mssql/server:2019-CU18-ubuntu-20.04")
+            .Build();
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            var connectionString = _msSqlcontainer.GetConnectionString();
+
+            builder.ConfigureTestServices(services =>
+            {
+                services.RemoveAll(typeof(DbContextOptions<OrganizadorContext>));
+                services.AddDbContext<OrganizadorContext>(options => options.UseSqlServer(connectionString));
+
+                var serviceProvider = services.BuildServiceProvider();
+
+                using var scope = serviceProvider.CreateScope();
+                var scopedServices = scope.ServiceProvider;
+                var context = scopedServices.GetRequiredService<OrganizadorContext>();
+                context.Database.EnsureCreated();
+            });
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _msSqlcontainer.StartAsync();
+        }
+
+        async Task IAsyncLifetime.DisposeAsync()
+        {
+            await _msSqlcontainer.DisposeAsync();
+        }
+    }
+}
---
+++ TrilhaApiDesafioTests/Factories/TarefaFactory.cs
@@ -0,0 +1,16 @@
+﻿using TrilhaApiDesafio.Models;
+
+namespace TrilhaApiDesafioTests.Factories
+{
+    public static class TarefaFactory
+    {
+        public static Tarefa CriarTarefa() =>
+            new()
+            {
+                Titulo = "Teste",
+                Descricao = "Teste de integração",
+                Data = DateTime.Now,
+                Status = EnumStatusTarefa.Pendente
+            };
+    }
+}
